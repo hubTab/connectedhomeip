373fbd10e6c34e6431dc39bf2cdaf56f66876160
Shana Azria <sazria@apple.com>
2021-04-21 00:58:44 +0200

    Fabrics server side logic

--- src/app/util/util.cpp
+++ src/app/util/util.cpp
@@ -145,6 +145,9 @@ void emberAfPluginDoorLockServerInitCallback(void);
 #ifdef ZCL_USING_DESCRIPTOR_CLUSTER_SERVER
 void emberAfPluginDescriptorServerInitCallback(void);
 #endif
+#ifdef ZCL_USING_OPERATIONAL_CREDENTIALS_CLUSTER_SERVER
+void emberAfPluginOperationalCredentialsServerInitCallback(void);
+#endif
 
 #ifdef EMBER_AF_GENERATED_PLUGIN_TICK_FUNCTION_DECLARATIONS
 EMBER_AF_GENERATED_PLUGIN_TICK_FUNCTION_DECLARATIONS
