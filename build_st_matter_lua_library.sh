7ffc4b1ed527bcc5e1c51ca4115959376d38d347
Eugen Feraru <eugen.feraru@smartthings.com>
2022-03-01 16:23:45 -0600

    Starting to prep the script for hubcore

--- build_st_matter_lua_library.sh
+++ build_st_matter_lua_library.sh
@@ -5,23 +5,37 @@
 # ----------------------------------------------------------------
 set -e
 
-THIS_DIR=$PWD
-DOC_DIR="$THIS_DIR/../hub-core/lib/scripting-engine/lua_libs/st/matter/generated/zap_clusters"
+THIS_DIR=$(dirname `which $0`)
+if [ $THIS_DIR == "." ]; then
+  THIS_DIR=`pwd` 
+fi
 
 COMPILED_GENERATED_ST_CLUSTERS=$THIS_DIR/zzz_generated/st-clusters
 GENERATED_ST_CLUSTERS=$COMPILED_GENERATED_ST_CLUSTERS/st/matter/generated/zap_clusters
+# DOC_DIR="../hub-core/lib/scripting-engine/lua_libs/st/matter/generate"
 
-doc_path="${1:-$DOC_DIR}"
+# doc_path="${1:-$DOC_DIR}"
+doc_path="${1}"
+
+echo "THIS_DIR: $THIS_DIR"
+echo "COMPILED_GENERATED_ST_CLUSTERS: $COMPILED_GENERATED_ST_CLUSTERS"
+echo "GENERATED_ST_CLUSTERS: $GENERATED_ST_CLUSTERS"
+echo "doc_path: $doc_path"
  
-[ ! -d "$doc_path" ] && { echo "$0 - Directory $doc_path not provided. Usage $0 <lua_library_directory>"; exit 1; }
+# [ ! -d "$doc_path" ] && { echo "$0 - Directory $doc_path not provided. Usage $0 <lua_library_directory>"; exit 1; }
+[ ! -d "$doc_path" ] && { mkdir -p "$doc_path"; }
 echo "Generating Matter Lua Library to $doc_path"
+
+cd $THIS_DIR
+
+
 
 # Start from smartthings_zap_artifacts branch
 git checkout smartthings_zap_artifacts
 
 # Temporary solution till we get these PRs merged into master
-git checkout DoorLock_Type_Update_Issue_15528 -- src/app/zap-templates/zcl/data-model/chip/door-lock-cluster.xml
-git checkout OnOff_Type_Update_Issue_15528 -- src/app/zap-templates/zcl/data-model/chip/onoff-cluster.xml
+# git checkout DoorLock_Type_Update_Issue_15528 -- src/app/zap-templates/zcl/data-model/chip/door-lock-cluster.xml
+# git checkout OnOff_Type_Update_Issue_15528 -- src/app/zap-templates/zcl/data-model/chip/onoff-cluster.xml
 
 # ###############################################################################
 # # Alternative approach: generate Lua Library in the master branch
@@ -36,7 +50,7 @@ git checkout OnOff_Type_Update_Issue_15528 -- src/app/zap-templates/zcl/data-mod
 ###############################################################################
 # Current approach: generate Lua Library in this branch
 ###############################################################################
-git rebase master
+# git rebase master
 
 # Generate the ZAP Compiled Cluster code
 mkdir -p $COMPILED_GENERATED_ST_CLUSTERS
